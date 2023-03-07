95b38e6ee54b0b4463bce4db477ecbe01bff0053
rodrigodeoliveira19 <oliveirarodrigo19.g@gmail.com>
2023-03-06 21:18:37 -0300

    Projeto frases do dia iniciado.

---
+++ frases_do_dia/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Frases do dia",
+    //Atributo para widgets
+    // home: Row(children: [
+    //   Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit ",
+    //     style: TextStyle(fontSize: 20),),
+    //   Text("T2: ")
+    // ],),
+    home: Container(
+      //color: Colors.white,
+      decoration:
+          BoxDecoration(border: Border.all(width: 3, color: Colors.cyan)),
+      margin: EdgeInsets.only(top: 40),
+      child: Column(
+        children: [Text("gagagg hahahhahha jajajjaja jajajjaja ajajajjsaj")],
+      ),
+    ),
+  ));
+}
