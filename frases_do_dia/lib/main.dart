8e9a2d06d068b34f6121a33d36cd81c0d7693a91
rodrigodeoliveira19 <oliveirarodrigo19.g@gmail.com>
2023-03-08 20:48:29 -0300

    64 Widgets - Stateless e Stateful

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+/* Stateless -> Widgets que não podem ser alterados(constates).
+   Stateful -> Widgets que podem ser alterados(Variaveis).
+* */
+
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: "Frases do dia",
     //Atributo para widgets
     // home: Row(children: [
@@ -9,20 +14,74 @@ void main() {
     //     style: TextStyle(fontSize: 20),),
     //   Text("T2: ")
     // ],),
-    home: Scaffold(
+    home: HomeStateful(),
+  ));
+}
+
+class HomeStateful extends StatefulWidget {
+  const HomeStateful({Key? key}) : super(key: key);
+
+  @override
+  State<HomeStateful> createState() => _HomeStatefulState();
+}
+
+class _HomeStatefulState extends State<HomeStateful> {
+  @override
+  Widget build(BuildContext context) {
+
+    var _titulo = "Instagram";
+
+    return Scaffold(
       //Barra superior do App
       appBar: AppBar(
-        title: Text("Instagram"),
+        title: Text(_titulo),
         backgroundColor: Colors.green,
       ),
       //Corpo / Area Central
-      body: Text("Add mensagem no body"),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.green,
-        child: Padding(
-          padding: EdgeInsets.all(16),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            FloatingActionButton(onPressed: (){
+              //Altera o estado dos componentes
+              setState(() {
+                _titulo = "Uber";
+              });
+            },
+                backgroundColor: Colors.lightBlue,
+                child: Text("Click AQ.")) ,
+
+          ],
         ),
-      ),
-    )
-  ));
+      )
+
+    );
+  }
 }
+
+
+// Conferir doc. Não rodou
+// class Home extends StatelessWidget {
+//   const Home({Key? key}) : super(key: key);
+//
+//   var _titulo = "Instagram";
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       //Barra superior do App
+//       appBar: AppBar(
+//         title: Text(_titulo),
+//         backgroundColor: Colors.green,
+//       ),
+//       //Corpo / Area Central
+//       body: Text("Add mensagem no body"),
+//       bottomNavigationBar: BottomAppBar(
+//         color: Colors.green,
+//         child: Padding(
+//           padding: EdgeInsets.all(16),
+//         ),
+//       ),
+//     );
+//
+//   }
+// }
