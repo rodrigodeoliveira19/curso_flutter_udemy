568c08c045080d0fdb4c08a6fa6d4e3afd657c78
rodrigodeoliveira19 <oliveirarodrigo19.g@gmail.com>
2023-03-12 15:32:06 -0300

    72 Interface alcool ou gasolina parte 2.

--- alcool_gasolina/lib/Home.dart
+++ alcool_gasolina/lib/Home.dart
@@ -21,41 +21,62 @@ class _HomeState extends State<Home> {
       ),
       body: Container(
         padding: EdgeInsets.all(32),
-        child: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(bottom: 32),
-              child: Image.asset("imagens/logo.png"),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Text(
-                "Saiba qual a melhor opção para abastecimento do seu carro.",
-                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+        //SingleChildScrollView - Define o Scroll de rolagem do aplicativo
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset("imagens/logo.png"),
               ),
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: "Preço álcool, ex: 1.59"),
-              style: TextStyle(fontSize: 22),
-              controller: _controllerAlcool,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(labelText: "Preço gasolina, ex: 2.59"),
-              style: TextStyle(fontSize: 22),
-              controller: _controllerGasolina,
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Column(
-                children: [
-                  ElevatedButton(onPressed: (){},
-                      child: Text("Calcular"))
-                ],
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Saiba qual a melhor opção para abastecimento do seu carro.",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
               ),
-            )
-          ],
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: "Preço álcool, ex: 1.59"),
+                style: TextStyle(fontSize: 22),
+                controller: _controllerAlcool,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(labelText: "Preço gasolina, ex: 2.59"),
+                style: TextStyle(fontSize: 22),
+                controller: _controllerGasolina,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: Column(
+                  //crossAxisAlignment - Faz com que os elementos ocupem o espaço completo da coluna.
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Calcular",
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blue,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  "Resultado",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
