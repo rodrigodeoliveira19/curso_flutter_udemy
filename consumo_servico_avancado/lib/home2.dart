7521d838d0ebdbd9cd41094665cde9e3c7050c89
rodrigodeoliveira19 <oliveirarodrigo19.g@gmail.com>
2023-05-10 21:07:38 -0300

    95 - Requisição Post.

--- consumo_servico_avancado/lib/home2.dart
+++ consumo_servico_avancado/lib/home2.dart
@@ -14,7 +14,6 @@ class Home2 extends StatefulWidget {
 
 class _HomeState2 extends State<Home2> {
 
-
   Future<List<Post>> _recuperarPostagens() async {
     String _url_Base = "https://jsonplaceholder.typicode.com";
     var uri = Uri.parse("$_url_Base/posts");
@@ -23,13 +22,32 @@ class _HomeState2 extends State<Home2> {
     var dadosJson = json.decode(response.body);
 
     List<Post> postagens = [];
-    for (var post in dadosJson){
+    for (var post in dadosJson) {
       print(post);
-      Post newPost = Post(post["userId"], post["id"], post["title"], post["body"]);
+      Post newPost =
+          Post(post["userId"], post["id"], post["title"], post["body"]);
       postagens.add(newPost);
     }
     return postagens;
+  }
 
+  _post() async{
+    String _url_Base = "https://jsonplaceholder.typicode.com";
+    var uri = Uri.parse("$_url_Base/posts");
+
+    http.Response response = await http.post(uri,
+        body: json.encode({
+          "userId": 1000000,
+          "id": null,
+          "title": "sunt",
+          "body": "ok"
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        });
+
+    print("reponse statusCode: ${response.statusCode}");
+    print("reponse body: ${response.body}");
   }
 
   @override
@@ -38,47 +56,87 @@ class _HomeState2 extends State<Home2> {
       appBar: AppBar(
         title: Text("Consumo de serviço avançado."),
       ),
-      body: FutureBuilder<List<Post>>(
-          future: _recuperarPostagens(),
-          //snapshot - permite acesso aos dados retornados em _recuperarPreco.
-          builder: (context, snapshot) {
-            String resultado;
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-                break;
-              case ConnectionState.active:
-              case ConnectionState.done:
-                print("conexao done");
-                if (snapshot.hasError) {
-                  resultado = "Erro ao carregar os dados.";
-                } else {
-                  return ListView.builder(
-                      itemCount: snapshot.data?.length,
-                      itemBuilder: (context, index){
+      body: Container(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: _post,
+                  child: Text(
+                    "Salvar",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Atualizar",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Deletar",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+            //Expanded - Informa ao objeto child qto de espaço ele deve ocupar na tela
+            Expanded(
+                child: FutureBuilder<List<Post>>(
+                  future: _recuperarPostagens(),
+              //snapshot - permite acesso aos dados retornados em _recuperarPreco.
+              builder: (context, snapshot) {
+                String resultado;
+                switch (snapshot.connectionState) {
+                  case ConnectionState.none:
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+                    break;
+                  case ConnectionState.active:
+                  case ConnectionState.done:
+                    print("conexao done");
+                    if (snapshot.hasError) {
+                      resultado = "Erro ao carregar os dados.";
+                    } else {
+                      return ListView.builder(
+                          itemCount: snapshot.data?.length,
+                          itemBuilder: (context, index) {
+                            List<Post>? lista = snapshot.data;
+                            Post post = lista![index];
 
-                        List<Post>? lista = snapshot.data;
-                        Post post = lista![index];
-
-                        return ListTile(
-                          title: Text( post.title ),
-                          subtitle: Text( post.id.toString() ),
-                        );
-
-                      }
-                  );
+                            return ListTile(
+                              title: Text(post.title),
+                              subtitle: Text(post.id.toString()),
+                            );
+                          });
+                    }
+                    break;
                 }
-                break;
-            }
 
-            return Center(
-              child: Text(resultado),
-            );
-          },
-          ),
+                return Center(
+                  child: Text(resultado),
+                );
+              },
+            ))
+          ],
+        ),
+      ),
     );
   }
 }
