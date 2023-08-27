f41336b6089197e511a6ce2471961d8c565b44f0
rodrigodeoliveira19 <oliveirarodrigo19.g@gmail.com>
2023-08-27 18:38:38 -0300

    Removendo, recuperando e escutando dados do Firebase.

--- firebase/lib/main.dart
+++ firebase/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() async{
+void main() async {
   print('Antes WidgetsFlutterBinding');
   //Inicializar Firebase
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,15 +19,46 @@ void main() async{
       .set({"Rodrigo": "300"});*/
 
   //Adiciona com identificador automaticamente
-  DocumentReference reference = await db.collection("noticias")
+  /*DocumentReference reference = await db.collection("noticias")
   .add(
       {
         "titulo": "Titulo Rodrigo",
         "ano": 2023
       }
   );
-  print('Executei');
+
   print("Result: "+reference.id);
+  */
+
+  //Deleta
+  //db.collection("usuarios").doc("pontuacao").delete();
+
+  //Get One
+  // DocumentSnapshot snapshot =
+  //     await db.collection("usuarios").doc("pontuacao_2").get();
+  // print('Dados: ${snapshot.data()}');
+  // var dados = snapshot.data() as Map;
+  // print("Valor : "+dados['Rodrigo']);
+
+  //Get All
+  // QuerySnapshot querySnapshot =
+  // await db.collection("noticias").get();
+  //
+  // for(QueryDocumentSnapshot item in querySnapshot.docs){
+  //   var dados = item.data() as Map;
+  //   print("item: "+dados['titulo'] );
+  // }
+
+  //Get all Listen - Fica escutando caso houver mudanças no banco
+  db.collection("usuarios").snapshots().listen((event) {
+    for(QueryDocumentSnapshot item in event.docs){
+      var dados = item.data() as Map;
+      print("usuario: "+dados['Rodrigo'] );
+    }
+  });
+
+
+  print('Executei');
 
   runApp(const MyApp());
 }
