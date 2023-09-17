d76540dd39cfe83a1cbc624f094975682f70901a
rodrigodeoliveira19 <oliveirarodrigo19.g@gmail.com>
2023-09-16 21:55:41 -0300

    Recuperando localização do usuario.

--- minhas_viagens/lib/Mapa.dart
+++ minhas_viagens/lib/Mapa.dart
@@ -2,6 +2,9 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
+
 
 class Mapa extends StatefulWidget {
   const Mapa({Key? key}) : super(key: key);
@@ -12,6 +15,10 @@ class Mapa extends StatefulWidget {
 
 class _MapaState extends State<Mapa> {
   Completer<GoogleMapController> _controller = Completer();
+  CameraPosition _posicaoCamera = CameraPosition(
+    target: LatLng(-23.563370, -46.652923),
+    zoom: 16,
+  );
 
   //Marcadores de locais no mapa
   Set<Marker> _marcadores = {};
@@ -20,38 +27,81 @@ class _MapaState extends State<Mapa> {
     _controller.complete(controller);
   }
 
-  _exibirMarcador(LatLng latLng){
-    Marker marcador = Marker(
-        markerId: MarkerId("marcador-${latLng.latitude}-${latLng.longitude}"),
-        position: latLng,
-        // infoWindow: InfoWindow(
-        //     title: "Shopping Cidade São Paulo"
-        // ),
-        // icon: BitmapDescriptor.defaultMarkerWithHue(
-        //     BitmapDescriptor.hueMagenta
-        // ),
-        // onTap: (){
-        //   print("Shopping clicado!!");
-        // }
-      //rotation: 45
-    );
+  _exibirMarcador(LatLng latLng) async{
 
-    setState(() {
-      _marcadores.add(marcador);
-    });
+    List<Placemark> placemarks = await placemarkFromCoordinates
+      (latLng.latitude, latLng.longitude);
+
+    if( placemarks != null && placemarks.length > 0 ) {
+      Placemark endereco = placemarks[0];
+
+      var rua = endereco.thoroughfare.toString();
+
+      Marker marcador = Marker(
+          markerId: MarkerId("marcador-${latLng.latitude}-${latLng.longitude}"),
+          position: latLng,
+        infoWindow: InfoWindow(
+          title: rua
+        )
+      );
+
+      setState(() {
+        _marcadores.add(marcador);
+      });
+    }
 
   }
+
+  _adicionarListenerLocalizacao() async{
+    LocationPermission permission;
+    permission = await Geolocator.requestPermission();
+
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+    StreamSubscription<Position> positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position position) {
+      print(position == null
+          ? 'Local: Unknown'
+          : 'Posicao atual: ${position.latitude.toString()}, ${position.longitude.toString()}');
+
+          setState(() {
+            _posicaoCamera = CameraPosition(
+              target: LatLng(position.latitude, position.longitude),
+              zoom: 16
+            );
+            _movimentarCamera();
+          });
+    });
+  }
+
+  _movimentarCamera() async {
+    GoogleMapController googleMapController = await _controller.future;
+    googleMapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+            _posicaoCamera
+        )
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _adicionarListenerLocalizacao();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+      ),
       body: Container(
         child: GoogleMap(
           mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(
-            target: LatLng(-23.563370, -46.652923),
-            zoom: 16,
-          ),
+          initialCameraPosition: _posicaoCamera,
           //Constroi o mapa
           onMapCreated: _onMapCreated,
           onLongPress: _exibirMarcador,
