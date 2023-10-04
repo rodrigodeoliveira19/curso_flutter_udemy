679202abd8b2e5ae9b7282a92e18ebd23598da7c
rodrigodeoliveira19 <oliveirarodrigo19.g@gmail.com>
2023-10-04 19:24:53 -0300

    Criando interface de viagens.

--- minhas_viagens/lib/uber/telas/PainelPassageiro.dart
+++ minhas_viagens/lib/uber/telas/PainelPassageiro.dart
@@ -4,6 +4,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 import 'dart:async';
+import 'dart:io';
 
 class PainelPassageiro extends StatefulWidget {
   const PainelPassageiro({Key? key}) : super(key: key);
@@ -13,10 +14,7 @@ class PainelPassageiro extends StatefulWidget {
 }
 
 class _PainelPassageiroState extends State<PainelPassageiro> {
-
-  List<String> itensMenu = [
-    "Configurações", "Deslogar"
-  ];
+  List<String> itensMenu = ["Configurações", "Deslogar"];
   Completer<GoogleMapController> _mapController = Completer();
   CameraPosition _posicaoCamera = CameraPosition(
     target: LatLng(-23.563370, -46.652923),
@@ -32,12 +30,12 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
     Navigator.pushReplacementNamed(context, "/");
   }
 
-  _escolhaMenuItem( String escolha ){
-    switch( escolha ){
-      case "Deslogar" :
+  _escolhaMenuItem(String escolha) {
+    switch (escolha) {
+      case "Deslogar":
         _deslogarUsuario();
         break;
-      case "Configurações" :
+      case "Configurações":
         break;
     }
   }
@@ -46,7 +44,7 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
     _mapController.complete(mapController);
   }
 
-  _adicionarListenerLocalizacao() async{
+  _adicionarListenerLocalizacao() async {
     LocationPermission permission;
     permission = await Geolocator.requestPermission();
 
@@ -64,9 +62,7 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
 
       setState(() {
         _posicaoCamera = CameraPosition(
-            target: LatLng(position.latitude, position.longitude),
-            zoom: 19
-        );
+            target: LatLng(position.latitude, position.longitude), zoom: 19);
         _movimentarCamera();
       });
     });
@@ -74,11 +70,8 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
 
   _movimentarCamera() async {
     GoogleMapController googleMapController = await _mapController.future;
-    googleMapController.animateCamera(
-        CameraUpdate.newCameraPosition(
-            _posicaoCamera
-        )
-    );
+    googleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(_posicaoCamera));
   }
 
   @override
@@ -95,33 +88,118 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
         actions: <Widget>[
           PopupMenuButton<String>(
             onSelected: _escolhaMenuItem,
-            itemBuilder: (context){
-
-              return itensMenu.map((String item){
-
+            itemBuilder: (context) {
+              return itensMenu.map((String item) {
                 return PopupMenuItem<String>(
                   value: item,
                   child: Text(item),
                 );
-
               }).toList();
-
             },
           )
         ],
       ),
       body: Container(
-        child: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: _posicaoCamera,
-          //Constroi o mapa
-          onMapCreated: _onMapCreated,
-          myLocationButtonEnabled: true,
-          myLocationEnabled: true,
-          //onLongPress: _adicionarMarcador,
-          //markers: _marcadores,
-        ),
-      ),
+          //Stack - Empilha os itens na ordem de inserção.
+          child: Stack(
+        children: [
+          GoogleMap(
+            mapType: MapType.normal,
+            initialCameraPosition: _posicaoCamera,
+            //Constroi o mapa
+            onMapCreated: _onMapCreated,
+            myLocationButtonEnabled: false,
+            myLocationEnabled: true,
+            zoomControlsEnabled: false,
+            //onLongPress: _adicionarMarcador,
+            //markers: _marcadores,
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.white),
+                child: TextField(
+                  readOnly: true,
+                  decoration: InputDecoration(
+                      icon: Container(
+                        margin: EdgeInsets.only(left: 20),
+                        width: 10,
+                        height: 10,
+                        child: Icon(
+                          Icons.location_on,
+                          color: Colors.green,
+                        ),
+                      ),
+                      hintText: "Meu local",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 15, top: 16)),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            top: 55,
+            left: 0,
+            right: 0,
+            child: Padding(
+              padding: EdgeInsets.all(10),
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.grey),
+                    borderRadius: BorderRadius.circular(3),
+                    color: Colors.white),
+                child: TextField(
+                  decoration: InputDecoration(
+                      icon: Container(
+                        margin: EdgeInsets.only(left: 20),
+                        width: 10,
+                        height: 10,
+                        child: Icon(
+                          Icons.local_taxi,
+                          color: Colors.black,
+                        ),
+                      ),
+                      hintText: "Digite o destino",
+                      border: InputBorder.none,
+                      contentPadding: EdgeInsets.only(left: 15, top: 16)),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: 0,
+            left: 0,
+            bottom: 0,
+            child: Padding(
+              padding: Platform.isIOS
+                  ? EdgeInsets.fromLTRB(20, 10, 20, 25)
+                  : EdgeInsets.all(10),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Chamar uber",
+                  style: TextStyle(fontSize: 20, color: Colors.white),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  padding: EdgeInsets.fromLTRB(32, 16, 32, 16),
+                ),
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
