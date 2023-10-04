d57080b5ffc077132bb6281393ef6220faf3f5e9
rodrigodeoliveira19 <oliveirarodrigo19.g@gmail.com>
2023-10-04 16:42:38 -0300

    Uber - 216 Configurando mapa com localização atual.

--- minhas_viagens/lib/uber/telas/PainelPassageiro.dart
+++ minhas_viagens/lib/uber/telas/PainelPassageiro.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
 class PainelPassageiro extends StatefulWidget {
   const PainelPassageiro({Key? key}) : super(key: key);
@@ -14,6 +17,11 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
   List<String> itensMenu = [
     "Configurações", "Deslogar"
   ];
+  Completer<GoogleMapController> _mapController = Completer();
+  CameraPosition _posicaoCamera = CameraPosition(
+    target: LatLng(-23.563370, -46.652923),
+    zoom: 16,
+  );
 
   _deslogarUsuario() async {
     WidgetsFlutterBinding.ensureInitialized();
@@ -32,6 +40,50 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
       case "Configurações" :
         break;
     }
+  }
+
+  _onMapCreated(GoogleMapController mapController) {
+    _mapController.complete(mapController);
+  }
+
+  _adicionarListenerLocalizacao() async{
+    LocationPermission permission;
+    permission = await Geolocator.requestPermission();
+
+    final LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high,
+      distanceFilter: 100,
+    );
+    StreamSubscription<Position> positionStream =
+    Geolocator.getPositionStream(locationSettings: locationSettings)
+        .listen((Position position) {
+      print(position == null
+          ? 'Local: Unknown'
+          : 'Posicao atual: ${position.latitude.toString()}, ${position.longitude.toString()}');
+
+      setState(() {
+        _posicaoCamera = CameraPosition(
+            target: LatLng(position.latitude, position.longitude),
+            zoom: 16
+        );
+        _movimentarCamera();
+      });
+    });
+  }
+
+  _movimentarCamera() async {
+    GoogleMapController googleMapController = await _mapController.future;
+    googleMapController.animateCamera(
+        CameraUpdate.newCameraPosition(
+            _posicaoCamera
+        )
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _adicionarListenerLocalizacao();
   }
 
   @override
@@ -57,7 +109,16 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
           )
         ],
       ),
-      body: Container(),
+      body: Container(
+        child: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: _posicaoCamera,
+          //Constroi o mapa
+          onMapCreated: _onMapCreated,
+          //onLongPress: _adicionarMarcador,
+          //markers: _marcadores,
+        ),
+      ),
     );
   }
 }
