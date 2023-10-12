4dfd6975cf4faebc1a8c8dfe395eca70735ad8ce
rodrigodeoliveira19 <oliveirarodrigo19.g@gmail.com>
2023-10-12 18:08:44 -0300

    Uber - 226 Listando requisições para o motorista.

--- minhas_viagens/lib/uber/telas/PainelMotorista.dart
+++ minhas_viagens/lib/uber/telas/PainelMotorista.dart
@@ -1,6 +1,11 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import '../util/StatusRequisicao.dart';
 
 class PainelMotorista extends StatefulWidget {
   const PainelMotorista({Key? key}) : super(key: key);
@@ -14,6 +19,8 @@ class _PainelMotoristaState extends State<PainelMotorista> {
   List<String> itensMenu = [
     "Configurações", "Deslogar"
   ];
+  final _streamController = StreamController<QuerySnapshot>.broadcast();
+  FirebaseFirestore db = FirebaseFirestore.instance;
 
   _deslogarUsuario() async {
     WidgetsFlutterBinding.ensureInitialized();
@@ -34,8 +41,45 @@ class _PainelMotoristaState extends State<PainelMotorista> {
     }
   }
 
+   _adicionarListenerRequisicoes(){ //Stream<QuerySnapshot>
+
+    final stream = db.collection("requisicoes")
+        .where("status", isEqualTo: StatusRequisicao.AGUARDANDO )
+        .snapshots();
+
+    stream.listen((dados){
+      _streamController.add( dados );
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _adicionarListenerRequisicoes();
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    var mensagemCarregando = Center(
+      child: Column(
+        children: <Widget>[
+          Text("Carregando requisições"),
+          CircularProgressIndicator()
+        ],
+      ),
+    );
+
+    var mensagemNaoTemDados = Center(
+      child: Text(
+        "Você não tem nenhuma requisição :( ",
+        style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold
+        ),
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Painel motorista"),
@@ -57,7 +101,60 @@ class _PainelMotoristaState extends State<PainelMotorista> {
           )
         ],
       ),
-      body: Container(),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: _streamController.stream,
+        builder: (context, snapshot){
+          switch( snapshot.connectionState ){
+            case ConnectionState.none:
+            case ConnectionState.waiting:
+              return mensagemCarregando;
+              break;
+            case ConnectionState.active:
+            case ConnectionState.done:
+
+              if( snapshot.hasError ){
+                return Text("Erro ao carregar os dados!");
+              }else {
+
+                if(snapshot.hasData){
+                  QuerySnapshot<Object?>? querySnapshot = snapshot.data;
+                  if( querySnapshot?.docs.length == 0 ){
+                    return mensagemNaoTemDados;
+                  }else{
+
+                    return ListView.separated(
+                        itemCount: querySnapshot!.docs.length,
+                        separatorBuilder: (context, indice) => Divider(
+                          height: 2,
+                          color: Colors.grey,
+                        ),
+                        itemBuilder: (context, indice){
+
+                          List<DocumentSnapshot> requisicoes = querySnapshot.docs.toList();
+                          DocumentSnapshot item = requisicoes[ indice ];
+
+                          String idRequisicao = item["id"];
+                          String nomePassageiro = item["passageiro"]["nome"];
+                          String rua = item["destino"]["rua"];
+                          String numero = item["destino"]["numero"];
+
+                          return ListTile(
+                            title: Text( nomePassageiro ),
+                            subtitle: Text("destino: $rua, $numero"),
+                          );
+
+                        }
+                    );
+
+                  }
+                }
+
+              }
+              break;
+          };
+          return Text("Erro ao carregar Stream de dados os dados!");
+        },
+      ),
     );
   }
 }
