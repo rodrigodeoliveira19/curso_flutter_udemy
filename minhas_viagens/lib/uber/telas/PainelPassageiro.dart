a67b2c992cc688a0c048131f1548d05658b42532
rodrigodeoliveira19 <oliveirarodrigo19.g@gmail.com>
2023-12-14 21:01:24 -0300

    229 - Recuperando a requisicao ativa do motorista - parte 2.

--- minhas_viagens/lib/uber/telas/PainelPassageiro.dart
+++ minhas_viagens/lib/uber/telas/PainelPassageiro.dart
@@ -234,6 +234,13 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
     });
   }
 
+  _statusACaminho() {
+    _exibirCaixaEnderecoDestino = false;
+    _alterarBotaoPrincipal("Motorista a caminho", Colors.grey, () {
+      // _cancelarUber();
+    });
+  }
+
   _cancelarUber() async {
     User? user = await UsuarioFirebase.getUsuarioAtual();
     FirebaseFirestore db = FirebaseFirestore.instance;
@@ -271,6 +278,7 @@ class _PainelPassageiroState extends State<PainelPassageiro> {
             _statusAguardando();
             break;
           case StatusRequisicao.A_CAMINHO:
+            _statusACaminho();
             break;
           case StatusRequisicao.VIAGEM:
             break;
